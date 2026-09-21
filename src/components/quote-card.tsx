fd14b3ba6e3fe9eb2a86e1f5979618f35f3c669c
gitampo <tamponi.fra@libero.it>
2026-09-21 03:53:32 +0200

    testo quote

--- src/components/quote-card.tsx
+++ src/components/quote-card.tsx
@@ -14,7 +14,9 @@ export function QuoteCard({ citazione }: Props) {
       style={styles.card}
       imageStyle={styles.texture}
     >
-      <ThemedText style={styles.testo}>"{citazione.testo}" {'\n'} {citazione.traduzione}</ThemedText>
+      <ThemedText style={styles.testo}>
+        "{citazione.testo}" {"\n"} {citazione.traduzione}
+      </ThemedText>
       <ThemedText style={styles.fonte}>-{citazione.fonte}</ThemedText>
     </ImageBackground>
   );
@@ -22,13 +24,23 @@ export function QuoteCard({ citazione }: Props) {
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: "#e7cd5ab4",
+    backgroundColor: "#514305b4",
     borderColor: "#ffec9884",
     borderRadius: 5,
     padding: 10,
     marginTop: 10,
   },
-  testo: { fontStyle: "italic", fontSize: 16, alignSelf: "flex-start" },
-  fonte: { fontSize: 13, marginTop: 8, alignSelf: "flex-end" },
+  testo: {
+    fontStyle: "italic",
+    fontSize: 16,
+    alignSelf: "flex-start",
+    color: "#ffe100",
+  },
+  fonte: {
+    fontSize: 13,
+    marginTop: 8,
+    alignSelf: "flex-end",
+    color: "#fefbfb",
+  },
   texture: { borderRadius: 5, opacity: 0.25 },
 });
